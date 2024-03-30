b12baad0b6897024bb2e70f18f4fa3c383b46ad4
ky0422 <ky0422a@gmail.com>
2024-03-30 21:37:31 +0900

    `update`: updated vite

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
     plugins: [react()],
     build: { outDir: './build' },
-    base: '/site/',
+    base: '/oscilloscope/',
 })
